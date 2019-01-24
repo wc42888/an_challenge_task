ebf3b695b81f40cf9cbb9e9329d4f137e8c139cc
chao wang <wc42888@hotmail.com>
2019-01-24 22:35:43 +1100

    added font constants

---
+++ src/constants/typography/font.js
@@ -0,0 +1,9 @@
+export const LARGE_SIZE_FONT = '24pt';
+export const NORMAL_SIZE_FONT = '12pt';
+export const SMALL_SIZE_FONT = '6pt';
+
+export const APP_FONT_FAMILTY = 'sans-serif';
+
+export const APP_FONT_WEIGHT = 'lighter';
+
+export const LIGHTER_FONT_COLOR = '#AAAAAA';
