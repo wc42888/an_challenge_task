946f4f8d16ff37a34aa2f7d68012512f4f2207b2
chao wang <wc42888@hotmail.com>
2019-01-25 15:56:37 +1100

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import store from '../redux/store';
+import Products from './Products';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>
-          test
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <BrowserRouter>
+            <div>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <Redirect
+                    component={Products}
+                    to="/products/page/1"
+                  />
+                )
+                }
+              />
+              <Route
+                exact
+                path="/products/page/:pageNumber"
+                render={({ match }) => (
+                  <Products
+                    pageNumber={Number(match.params.pageNumber)}
+                  />
+                )}
+              />
+            </div>
+          </BrowserRouter>
+        </div>
+      </Provider>
     );
   }
 }
