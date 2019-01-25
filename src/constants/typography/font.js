c3d0ff90b1b49e5a40448c759459ba48b960f2b4
chao wang <wc42888@hotmail.com>
2019-01-25 15:54:40 +1100

    fixed typo

--- src/constants/typography/font.js
+++ src/constants/typography/font.js
@@ -2,7 +2,7 @@ export const LARGE_SIZE_FONT = '24pt';
 export const NORMAL_SIZE_FONT = '12pt';
 export const SMALL_SIZE_FONT = '6pt';
 
-export const APP_FONT_FAMILTY = 'sans-serif';
+export const APP_FONT_FAMILY = 'sans-serif';
 
 export const APP_FONT_WEIGHT = 'lighter';
 
