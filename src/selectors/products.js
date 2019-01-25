5d3b4df8203954c67515468a7cbe9e9cc38fc68a
chao wang <wc42888@hotmail.com>
2019-01-25 15:54:53 +1100

    updated products selector

--- src/selectors/products.js
+++ src/selectors/products.js
@@ -1,14 +1,15 @@
 import { createSelector } from 'reselect';
 
 const getAllProducts = state => state.get('products');
-const getCurrentPageNumber = state => state.get('currentPageNumber');
 const getItemsPerpage = state => state.get('itemsPerPage');
 
 export const getDisplayedProducts = createSelector(
-  [getAllProducts, getCurrentPageNumber, getItemsPerpage],
-  (products, currentPageNumber, itemsPerPage) => {
-    const begin = (currentPageNumber - 1) * itemsPerPage;
-    const end = currentPageNumber * itemsPerPage;
+  getAllProducts,
+  getItemsPerpage,
+  (_, props) => props.pageNumber,
+  (products, itemsPerPage, pageNumber) => {
+    const begin = (pageNumber - 1) * itemsPerPage;
+    const end = pageNumber * itemsPerPage;
     return products.slice(begin, end);
   },
 );
