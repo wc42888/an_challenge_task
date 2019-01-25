0a2e47b8ae52aa736ade89255a0f24a1f2b42ff3
chao wang <wc42888@hotmail.com>
2019-01-25 15:51:53 +1100

    removed current page number actions and reducers

--- src/redux/actions/currentPageNumber.js
+++
@@ -1,8 +0,0 @@
-export const SET_CURRENT_PAGE_NUMBER = 'SET_CURRENT_PAGE_NUMBER';
-
-export const setCurrentPageNumber = pageNumber => ({
-  type: SET_CURRENT_PAGE_NUMBER,
-  payload: {
-    pageNumber,
-  },
-});
